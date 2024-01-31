04a4cd8a74a297567825bf371d8eeaa13e97e63c
SidGames5 <saytleader@outlook.com>
2024-01-30 20:08:02 -0500

    Add config manager

---
+++ runner/Config.hx
@@ -0,0 +1,13 @@
+package;
+
+typedef Config = {
+	servers:{
+		http_port:Int, https_port:Int, db:{
+			db_enable:Bool, db_host:String, db_port:Int, db_key:String
+		}, sso:{
+			sso_enable:Bool, sso_host:String, sso_http:Bool, sso_http_port:Int, sso_https:Bool, sso_https_port:Int
+		}, ldap:{
+			ldap_enable:Bool, ldap_host:String, ldap_port:Int
+		}
+	}
+}
---
+++ runner/ConfigManager.hx
@@ -0,0 +1,48 @@
+import haxe.Json;
+import sys.io.File;
+import sys.FileSystem;
+
+class ConfigManager {
+	public static function init() {
+		var dc:Config = {
+			servers: {
+				http_port: 80,
+				https_port: 443,
+				db: {
+					db_enable: true,
+					db_host: "db.onlyauth.local",
+					db_port: 4487,
+					db_key: ""
+				},
+				sso: {
+					sso_enable: true,
+					sso_host: "sso.onlyauth.local",
+					sso_http: true,
+					sso_http_port: 80,
+					sso_https: true,
+					sso_https_port: 443
+				},
+				ldap: {
+					ldap_enable: true,
+					ldap_host: "ldap.onlyauth.local",
+					ldap_port: 389
+				}
+			}
+		};
+		File.saveContent("/etc/onlyauth/config.json", Json.stringify(dc));
+	}
+
+	public static function check():Bool {
+		return FileSystem.exists("/etc/onlyauth/config.json");
+	}
+
+	public static function read():Config {
+		if (!check())
+			init();
+		return Json.parse(File.getContent("/etc/onlyauth/config.json"));
+	}
+
+	public static function write(config:Config) {
+		File.saveContent("/etc/onlyauth/config.json", Json.stringify(config));
+	}
+}
