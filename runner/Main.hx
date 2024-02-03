cb63bbaab9be1d4cd77fcbe8e12119f9ff20b915
SidGames5 <saytleader@outlook.com>
2024-02-03 14:40:45 -0500

    Start the db thread

--- runner/Main.hx
+++ runner/Main.hx
@@ -1,8 +1,16 @@
+import sys.io.Process;
+import sys.thread.Thread;
+
 class Main {
 	public static function main() {
 		if (!ConfigManager.check()) {
 			Sys.println("Initializing config");
 			ConfigManager.init();
 		}
+
+		var config = ConfigManager.read();
+
+		Sys.println("Starting database");
+		var threadDb = new Process("/usr/lib/onlyauth/modules/db.oam", [], true);
 	}
 }
